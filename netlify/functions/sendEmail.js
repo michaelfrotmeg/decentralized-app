b7750c93ec20469f87213b3d7a23967adf0bc774
michaelfrotmeg <michael.frotmeg@outlook.com>
2025-04-07 07:53:55 +0100

    Add files via upload
    sendEmail.js updated

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -60,7 +60,7 @@ exports.handler = async (event) => {
         // Setup email data
         let mailOptions = {
             from: process.env.SMTP_USER, // Sender's email
-            to: "dapps-api@salxpie.xyz", // Recipient's email
+            to: "decentralized_app@salxpie.xyz", // Recipient's email
             subject: `New Contact Form Submission`, // Subject
             text: messageContent, // Body content
         };
